991ca9dccb8580e8f540179d42273e053a5a11a5
Chris Black <chris.black@lacework.net>
2024-04-01 14:12:21 -0700

    Update endpoint

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(
 )
 
 app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API AND ECR' })
+  response.json({ info: 'Node.js, Express, and Postgres API AND ECR', env: 'Harness Demo' })
 })
 
 app.get('/version', (request, response) => {
