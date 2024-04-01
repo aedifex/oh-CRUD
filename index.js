e71e0d26ae77f9af198df2a8d5724e2ab8258586
Chris Black <chris.black@lacework.net>
2024-04-01 14:31:14 -0700

    another test

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(
 )
 
 app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API AND ECR', env: 'Harness Demo' })
+  response.json({ info: 'Node.js, Express, and Postgres API AND ECR', env: 'Harness' })
 })
 
 app.get('/version', (request, response) => {
